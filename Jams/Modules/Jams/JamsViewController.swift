05edbb2f46cc84248d494a97faf124ab6634afcf
Jaja Yting <jlyting@icloud.com>
2021-10-06 21:33:24 +0800

    Assign search controller in the navigation item of the Jams

--- Jams/Modules/Jams/JamsViewController.swift
+++ Jams/Modules/Jams/JamsViewController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class JamsViewController: UIViewController {
-
+    
+    // MARK: - Fields
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+     
+        self.navigationItem.searchController = self.searchController
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
