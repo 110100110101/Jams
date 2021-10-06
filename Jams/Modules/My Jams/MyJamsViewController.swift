bb90a220f0662a2a19dd4d098acdc8228675d5ca
Jaja Yting <jlyting@icloud.com>
2021-10-06 21:45:55 +0800

    Add view and controller for the My Jams module

---
+++ Jams/Modules/My Jams/MyJamsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyJamsViewController.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/6/21.
+//
+
+import UIKit
+
+class MyJamsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
