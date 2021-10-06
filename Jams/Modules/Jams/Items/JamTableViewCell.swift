f1be562a64b2eb2c6e9cca781e062204be686396
Jaja Yting <jlyting@icloud.com>
2021-10-06 22:48:20 +0800

    Reset the data assigned to prevent data from other jams to leak on other instances, upon prepareForReuse

--- Jams/Modules/Jams/Items/JamTableViewCell.swift
+++ Jams/Modules/Jams/Items/JamTableViewCell.swift
@@ -15,6 +15,19 @@ class JamTableViewCell: UITableViewCell {
     @IBOutlet private var labelTrackName: UILabel!
     @IBOutlet private var labelGenre: UILabel!
     @IBOutlet private var buttonPurchase: UIButton! // This button also serves the price tag for the track
+    
+    // MARK: - View Lifecycle
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        // Reset the data assigned to prevent data from other jams to leak on other instances
+        
+        self.imageViewTrackArtwork.image = nil
+        self.labelTrackName.text = nil
+        self.labelGenre.text = nil
+        self.buttonPurchase.setTitle(nil, for: .normal)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
