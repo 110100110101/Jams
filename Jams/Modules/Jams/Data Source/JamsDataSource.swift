1d2154243e0ba5acbdfe6cb163df76ae856496c6
Jaja Yting <jlyting@icloud.com>
2021-10-09 16:52:03 +0800

    Use moya as the backend for search, invoke search

--- Jams/Modules/Jams/Data Source/JamsDataSource.swift
+++ Jams/Modules/Jams/Data Source/JamsDataSource.swift
@@ -6,14 +6,33 @@
 //
 
 import Foundation
+import Moya
+import RxSwift
 
 final class JamsDataSource: JamsViewModelDataSource {
+    
+    private let provider = MoyaProvider<ITunes>()
+    
+    private let disposeBag = DisposeBag()
     
     // MARK: - JamsViewModelDataSource Methods
     
     func search(jam: String, completion: @escaping ([Any]?, Error?) -> ()) {
-        // TODO: Search it properly!
-        completion([], nil)
+            
+        let parameters = ["term": jam,
+                          "entity": "movie"]
+        
+        self.provider
+            .rx
+            .request(.search(parameters: parameters))
+            .filterSuccessfulStatusCodes()
+            .subscribe(onSuccess: { (response) in
+                // TODO: Decode the data
+                completion([], nil)
+            }, onFailure: { (error) in
+                completion(nil, error)
+            })
+            .disposed(by: self.disposeBag)
     }
     
     func toggleFavorite(_ isFavorite: Bool, jam: Any) {
