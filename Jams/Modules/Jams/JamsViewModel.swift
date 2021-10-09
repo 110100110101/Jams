2a8eed83fb792ba73beb3249cb4a0539a6bb891d
Jaja Yting <jlyting@icloud.com>
2021-10-09 13:56:08 +0800

    Declare known observable fields for Jams' view model

--- Jams/Modules/Jams/JamsViewModel.swift
+++ Jams/Modules/Jams/JamsViewModel.swift
@@ -6,7 +6,19 @@
 //
 
 import Foundation
+import RxCocoa
 
 class JamsViewModel {
     
+    // MARK: - Observable Fields
+    
+    /**
+     Field which the consumer may observe to get the jams. Search results would be also routed here.
+     */
+    public let jams = BehaviorRelay<[Any]>(value: []) // TODO: Declare proper type
+    
+    /**
+     Determines whether there's an error occurred while searching for jams
+     */
+    public let hasEncounteredAnErrorWhileSearching = BehaviorRelay<Bool>(value: false)
 }
