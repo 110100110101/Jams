447f8253d6dccecb2659b32a52c9b636e66d2fad
Jaja Yting <jlyting@icloud.com>
2021-10-09 15:52:09 +0800

    Make Itunes conform to TargetType protocol

--- Jams/Modules/Network/ITunes.swift
+++ Jams/Modules/Network/ITunes.swift
@@ -9,5 +9,35 @@ import Foundation
 import Moya
 
 enum ITunes {
-    case search(term: String)
+    case search(parameters: [String : Any])
+}
+
+extension ITunes: TargetType {
+    
+    var baseURL: URL {
+        return URL(string: "https://itunes.apple.com")!
+    }
+    
+    var path: String {
+        return "/search"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Task {
+        switch self {
+        case .search(let parameters):
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+    var sampleData: Data {
+        return Data() // TODO: Try to simulate this
+    }
 }
