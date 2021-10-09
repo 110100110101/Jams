93e13dc6c93dba0dd47c9193c314972f6cc94741
Jaja Yting <jlyting@icloud.com>
2021-10-09 15:57:10 +0800

    Make the JamsDataSource conform to JamsViewModelDataSource

--- Jams/Modules/Jams/Data Source/JamsDataSource.swift
+++ Jams/Modules/Jams/Data Source/JamsDataSource.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-final class JamsDataSource {
+final class JamsDataSource: JamsViewModelDataSource {
     
+    // MARK: - JamsViewModelDataSource Methods
+    
+    func search(jam: String, completion: @escaping ([Any]?, Error?) -> ()) {
+        // TODO: Search it properly!
+        completion([], nil)
+    }
+    
+    func toggleFavorite(_ isFavorite: Bool, onJam jam: Any) {
+        // TODO: Toggle it
+    }
 }
