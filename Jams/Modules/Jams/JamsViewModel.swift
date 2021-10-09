28e7fcb58d1bed4db3767115fd071de601cfff34
Jaja Yting <jlyting@icloud.com>
2021-10-09 14:42:19 +0800

    Declare protocol for the data source for the JamsViewModel

--- Jams/Modules/Jams/JamsViewModel.swift
+++ Jams/Modules/Jams/JamsViewModel.swift
@@ -8,6 +8,25 @@
 import Foundation
 import RxCocoa
 
+/**
+ The methods that an object adopts to manage and provide jams for Jams' ViewModel
+ */
+protocol JamsViewModelDataSource {
+    
+    /**
+     Asks the data source to search for Jams
+     
+     - parameter jam: Name of the Jam
+     - parameter completion: Invoked whenever the data source has finished searching for it. Only one of the argument should contain a value.
+     */
+    func search(jam: String, completion: @escaping ([Any]?, Error?) -> ()) // TODO: Provide proper type for `Any`
+    
+    /**
+     Asks the data source to add/remove the Jam on favorites
+     */
+    func toggleFavorite(_ isFavorite: Bool, onJam jam: Any) // TODO: Provider type for `Any`
+}
+
 class JamsViewModel {
     
     // MARK: - Observable Fields
