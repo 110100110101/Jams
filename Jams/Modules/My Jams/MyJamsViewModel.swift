c8829fbd4d9d10474acd789fca478d09b30b64b1
Jaja Yting <jlyting@icloud.com>
2021-10-10 20:34:40 +0800

    Declare protocol which manages the data of the My Jams view

--- Jams/Modules/My Jams/MyJamsViewModel.swift
+++ Jams/Modules/My Jams/MyJamsViewModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+/**
+ A data source object which manages the data of the My Jams View
+ */
+protocol MyJamsViewModelDataSource {
+    
+    /**
+     Asks the data source for all the Favorite Jams of the user
+     */
+    func getAllFavoriteJams(completion: @escaping ([FavoriteJam]?, Error) -> ())
+    
+    /**
+     Asks the data source to remove the jam on the list
+     */
+    func remove(jam: FavoriteJam)
+}
+
 class MyJamsViewModel {
     
 }
