a9b016a5316375decb9bae6ed1a391f905fba0e3
Jaja Yting <jlyting@icloud.com>
2021-10-10 17:53:18 +0800

    Add core data manager for jams

---
+++ Jams/Modules/Core Data/CoreDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataManager.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/10/21.
+//
+
+import Foundation
+
+final class CoreDataManager {
+    
+    public static let sharedInstance = CoreDataManager()
+    
+    private init() {}
+}
