d9e5f58b9249abcf30e2d20606f71384d1dc6b05
Jaja Yting <jlyting@icloud.com>
2021-10-10 18:06:20 +0800

    Add track ID on model and entity

--- Jams/Modules/Models/FetchedJam.swift
+++ Jams/Modules/Models/FetchedJam.swift
@@ -12,6 +12,7 @@ import Foundation
  */
 class FetchedJam: Decodable {
     
+    public var trackId: Decimal
     public var trackName: String
     public var trackArtwork: URL
     public var trackLongDescription: String
@@ -22,6 +23,7 @@ class FetchedJam: Decodable {
         
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
+        self.trackId = try container.decode(Decimal.self, forKey: .trackId)
         self.trackName = try container.decode(String.self, forKey: .trackName)
         self.trackArtwork = try container.decode(URL.self, forKey: .trackArtwork)
         self.trackLongDescription = try container.decode(String.self, forKey: .trackLongDescription)
@@ -30,6 +32,7 @@ class FetchedJam: Decodable {
     }
     
     private enum CodingKeys: String, CodingKey {
+        case trackId = "trackId"
         case trackName
         case trackArtwork = "artworkUrl100"
         case trackLongDescription = "longDescription"
