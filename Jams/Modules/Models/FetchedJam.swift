92ee4c82f83f5c040006ebca696f04c7b6555bac
Jaja Yting <jlyting@icloud.com>
2021-10-10 12:35:48 +0800

    Add model to represent the results from the iTunes search API

---
+++ Jams/Modules/Models/FetchedJam.swift
@@ -0,0 +1,15 @@
+//
+//  FetchedJam.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/10/21.
+//
+
+import Foundation
+
+/**
+ Object that represents the Jam fetched from the iTunes Search API
+ */
+class FetchedJam {
+    
+}
