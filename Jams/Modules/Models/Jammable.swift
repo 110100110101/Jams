03fea906be4801e72ad586e73811849e0aef41cc
Jaja Yting <jlyting@icloud.com>
2021-10-10 18:54:25 +0800

    Declare protocol which defines what jams are

---
+++ Jams/Modules/Models/Jammable.swift
@@ -0,0 +1,20 @@
+//
+//  Jammable.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/10/21.
+//
+
+import Foundation
+
+/**
+ Suite of fields that defines what jams are
+ */
+protocol Jammable {
+    
+    var jamID: Decimal { get }
+    var jamName: String { get }
+    var jamArtwork: URL { get }
+    var jamDescription: String { get }
+    var jamGenre: String { get }
+}
