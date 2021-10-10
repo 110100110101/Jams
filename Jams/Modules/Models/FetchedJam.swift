41200b694654265910dbb39890b958a3b6d36c39
Jaja Yting <jlyting@icloud.com>
2021-10-10 12:42:18 +0800

    Declare FetchedJam's known fields and conform to Decodable protocol

--- Jams/Modules/Models/FetchedJam.swift
+++ Jams/Modules/Models/FetchedJam.swift
@@ -10,6 +10,29 @@ import Foundation
 /**
  Object that represents the Jam fetched from the iTunes Search API
  */
-class FetchedJam {
+class FetchedJam: Decodable {
     
+    public var trackName: String
+    public var trackArtwork: URL
+    public var genre: String
+    public var price: Decimal
+    public var isFavorite: Bool
+    
+    required init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.trackName = try container.decode(String.self, forKey: .trackName)
+        self.trackArtwork = try container.decode(URL.self, forKey: .trackArtwork)
+        self.genre = try container.decode(String.self, forKey: .genre)
+        self.price = try container.decode(Decimal.self, forKey: .price)
+        self.isFavorite = false
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case trackName
+        case trackArtwork
+        case genre
+        case price
+    }
 }
