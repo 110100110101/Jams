4da0b7cfb671034e2c9f479b1dd19fb295bc156b
Jaja Yting <jlyting@icloud.com>
2021-10-10 20:38:17 +0800

    Add designated data source for My Jams View

---
+++ Jams/Modules/My Jams/Data Source/MyJamsDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  MyJamsDataSource.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/10/21.
+//
+
+import Foundation
+
+/**
+ Designated data source for the My Jams View
+ */
+final class MyJamsDataSource: MyJamsViewModelDataSource {
+    
+    // MARK: - MyJamsViewModelDataSource Methods
+    
+    func getAllFavoriteJams(completion: @escaping ([FavoriteJam]?, Error) -> ()) {
+        
+    }
+    
+    func remove(jam: FavoriteJam) {
+        
+    }
+}
