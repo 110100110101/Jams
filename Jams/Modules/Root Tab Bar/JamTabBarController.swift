f3cf38a837ae4e41e2cb3dff5dce601b79e6e377
Jaja Yting <jlyting@icloud.com>
2021-10-11 21:08:22 +0800

    Encode and decode the selected index

--- Jams/Modules/Root Tab Bar/JamTabBarController.swift
+++ Jams/Modules/Root Tab Bar/JamTabBarController.swift
@@ -7,5 +7,24 @@
 
 import UIKit
 
+fileprivate enum JamTabBarControllerRestorationKeys {
+    static let selectedIndex = "selectedIndex"
+}
+
 class JamTabBarController: UITabBarController {
+    
+    // MARK: - UIStateRestoring Methods
+    
+    override func encodeRestorableState(with coder: NSCoder) {
+        super.encodeRestorableState(with: coder)
+        
+        coder.encode(self.selectedIndex, forKey: JamTabBarControllerRestorationKeys.selectedIndex)
+    }
+    
+    override func decodeRestorableState(with coder: NSCoder) {
+        super.decodeRestorableState(with: coder)
+        
+        let selectedIndex = coder.decodeInteger(forKey: JamTabBarControllerRestorationKeys.selectedIndex)
+        self.selectedIndex = selectedIndex
+    }
 }
