7a321f9ef75967725f9851cc6a7cb7eb832880fc
Jaja Yting <jlyting@icloud.com>
2021-10-11 15:12:49 +0800

    Add designated view model for jam details view

---
+++ Jams/Modules/Jam Details/JamDetailsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  JamDetailsViewModel.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/11/21.
+//
+
+import Foundation
+
+/**
+ Designated view model for the Jam Details view
+ */
+class JamDetailsViewModel {
+    
+}
