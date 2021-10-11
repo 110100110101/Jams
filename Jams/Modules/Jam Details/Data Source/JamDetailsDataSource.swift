58342c6c8bfbfdf29954d00115b756304e134d0d
Jaja Yting <jlyting@icloud.com>
2021-10-11 15:41:28 +0800

    Add designated data source for Jam Details view

---
+++ Jams/Modules/Jam Details/Data Source/JamDetailsDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  JamDetailsDataSource.swift
+//  Jams
+//
+//  Created by Jaja Yting on 10/11/21.
+//
+
+import Foundation
+
+/**
+ Designated data source for Jam Details View
+ */
+class JamDetailsDataSource: JamDetailsViewModelDataSource {
+    
+    // MARK: - JamDetailsViewModelDataSource Methods
+    
+    func addJamToFavorites(_ jam: Jammable, completion: @escaping (Bool) -> ()) {
+        
+    }
+    
+    func removeJamOnFavorites(_ jam: Jammable, completion: @escaping (Bool) -> ()) {
+        
+    }
+}
